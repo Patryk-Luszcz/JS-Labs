8066e482d938d13bc7beb0d3d0e024351118b774
patrykluszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2024-02-05 21:01:14 +0100

    feat: implement logic for drumkit

--- lab3/drumkit.js
+++ lab3/drumkit.js
@@ -1,22 +1,100 @@
-document.addEventListener('keypress', onKeyPress);
+const recordBtns = [...document.querySelectorAll('.record')];
+const playBtns = [...document.querySelectorAll('.play')];
+const checkboxes = [...document.querySelectorAll('.checkbox')];
+const playAllBtn = document.querySelector('.playAll');
+const playSelectedBtn = document.querySelector('.playSelected');
+
+let isRecording = false;
+const channels = new Array(4).fill().map(() => ({ endTime: 0, soundTrack: [], checked: false }));
 
 const KeyToSound = {
-	a: document.querySelector('#s1'),
-	s: document.querySelector('#s2'),
-	d: document.querySelector('#s3'),
-	f: document.querySelector('#s4'),
-	g: document.querySelector('#s5'),
-	h: document.querySelector('#s6'),
-	j: document.querySelector('#s7'),
-	k: document.querySelector('#s8'),
-	l: document.querySelector('#s9'),
+	a: '#s1',
+	s: '#s2',
+	d: '#s3',
+	f: '#s4',
+	g: '#s5',
+	h: '#s6',
+	j: '#s7',
+	k: '#s8',
+	l: '#s9',
 };
 
-function onKeyPress(event) {
-	const sound = KeyToSound[event.key];
-	playSound(sound);
-}
-function playSound(sound) {
-	sound.currentTime = 0;
-	sound.play();
-}
+const playSound = (soundId) => {
+	const soundElement = document.querySelector(soundId);
+	soundElement.play();
+};
+
+const onKeyPress = (channel) => (event) => {
+	const soundId = KeyToSound[event.key];
+	if (soundId) {
+		const sound = { soundId, timeStamp: Date.now() };
+
+		channel.soundTrack.push(sound);
+
+		playSound(soundId);
+	}
+};
+
+const recordTrack = (channel, recordBtn) => {
+	isRecording = !isRecording;
+	recordBtn.textContent = isRecording ? 'Stop Record' : 'Start Record';
+
+	if (isRecording) {
+		channel.soundTrack = [];
+	}
+
+	if (isRecording) {
+		document.addEventListener('keypress', onKeyPress(channel));
+
+		channel.stopRecording = () => {
+			document.removeEventListener('keypress', onKeyPress(channel));
+			channel.endTime = Date.now();
+		};
+	} else {
+		channel.stopRecording();
+	}
+};
+
+const playTrack = (playBtn, channel) => {
+	const { soundTrack, endTime } = channel;
+
+	if (soundTrack.length > 0) {
+		const oldTextContent = playBtn.textContent;
+		playBtn.textContent = 'Playing...';
+		let lastSoundTime = 0;
+
+		soundTrack.forEach(({ soundId, timeStamp }) => {
+			const timeToPlay = endTime - timeStamp;
+
+			setTimeout(() => {
+				playSound(soundId);
+			}, timeToPlay);
+
+			if (timeToPlay > lastSoundTime) {
+				lastSoundTime = timeToPlay;
+			}
+		});
+
+		setTimeout(() => {
+			playBtn.textContent = oldTextContent;
+		}, lastSoundTime);
+	}
+};
+
+const checkChannel = (channel) => (channel.checked = !channel.checked);
+
+recordBtns.forEach((recordBtn, index) => {
+	recordBtn.addEventListener('click', () => recordTrack(channels[index], recordBtn));
+});
+
+playBtns.forEach((playBtn, index) => {
+	playBtn.addEventListener('click', () => playTrack(playBtn, channels[index]));
+});
+
+checkboxes.forEach((checkbox, index) => checkbox.addEventListener('click', () => checkChannel(channels[index])));
+
+playAllBtn.addEventListener('click', () => channels.forEach((channel) => playTrack(playAllBtn, channel)));
+
+playSelectedBtn.addEventListener('click', () =>
+	channels.forEach((channel) => channel.checked && playTrack(playSelectedBtn, channel))
+);
